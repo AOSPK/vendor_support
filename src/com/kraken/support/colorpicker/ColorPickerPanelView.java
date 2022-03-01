a3b42e6d0742f52c3c62b392d0bea5b6f8d9c990
Alexandre Rangel <mamutal91@gmail.com>
2022-03-01 05:02:54 -0300

    Switch to our project
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>
    Change-Id: I5c23dc6d84b9372a8ca6d5571208083732835294

--- src/com/kraken/support/colorpicker/ColorPickerPanelView.java
+++ src/com/kraken/support/colorpicker/ColorPickerPanelView.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package com.arrow.support.colorpicker;
+package com.kraken.support.colorpicker;
 
 import android.content.Context;
 import android.graphics.Canvas;
